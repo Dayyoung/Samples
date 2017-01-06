c4cced05c29516563838c180fab22a71a73ec803
Greg you <greg@okthere.com>
2017-01-06 20:10:41 +0900

    Add files.

---
+++ Material/Programmatic/PageTabBarController/Pods/Target Support Files/Pods-PageTabBarController/Pods-PageTabBarController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PageTabBarController : NSObject
+@end
+@implementation PodsDummy_Pods_PageTabBarController
+@end
---
+++ Material/Storyboards/NavigationDrawerController/Pods/Target Support Files/Pods-NavigationDrawerController/Pods-NavigationDrawerController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NavigationDrawerController : NSObject
+@end
+@implementation PodsDummy_Pods_NavigationDrawerController
+@end
